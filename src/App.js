3667aa78b18bd19476fdad1edf948f1201f50736
Sean <seahnnnnnn@gmail.com>
2024-01-27 21:07:58 +0900

    Feat: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomaPage from "./routes/HomePage";
+import SignUpPage from "./routes/SignUpPage";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HomaPage />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomaPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
