79a14d60532aa682c087be00dcdfe02030451961
Sean <seahnnnnnn@gmail.com>
2024-01-28 22:22:09 +0900

    Feat: button

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
+// 개발용으로 detail로 가는 버튼 추가해둠. 삭제하고 시작하길 바람!
 const HomaPage = () => {
-    return <div>멋사 블로그의 홈 화면입니다!</div>;
+    return (
+        <div>
+            <Link to="/2">버튼</Link>
+        </div>
+    );
 };
 
 export default HomaPage;
