8a70967973d561b5c65f2fb96ecb52b481e74d92
Sean <seahnnnnnn@gmail.com>
2024-02-02 22:09:47 +0900

    Feat: link to postdetail (in smallpost)

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const SmallPost = ({ post }) => {
   const onClickLike = () => {
     console.log("나도 좋아!"); // add api call for liking post here
   };
 
   return (
-    <div className="w-64 relative block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-2 border-box border-white hover:bg-orange-400 hover:text-black hover:border-transparent hover:ring-orange-300 hover:ring-opacity-90 rounded-xl font-medium">
+    <Link to={`/${post.id}`} className="w-64 relative block group py-10 px-8 mr-5 my-5 ring-8 ring-transparent border-2 border-box border-white hover:bg-orange-400 hover:text-black hover:border-transparent hover:ring-orange-300 hover:ring-opacity-90 rounded-xl font-medium">
       <h1 className="font-extrabold text-2xl truncate">{post.title}</h1>
       <p className="mt-2">{post.author.username}</p>
       <div className="flex flex-wrap mt-4">
@@ -17,7 +19,7 @@ export const SmallPost = ({ post }) => {
       <div onClick={onClickLike}>
         {post.like_users.length > 0 && `❤️ ${post.like_users.length}`}
       </div>
-    </div>
+    </Link>
   );
 };
 
