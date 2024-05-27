ebea293d09f70503747c354dee0ed86aa8b3ef26
Taehyeungkim <kth1837@snu.ac.kr>
2024-05-27 19:29:29 +0900

    :sparkles: getting comments for each post

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,53 +1,79 @@
-import { useState } from "react";
-import comments from "../../data/comments"; // dummy data
+import { useState, useEffect } from "react";
+import { getComments } from "../../apis/api";
 import CommentElement from "./CommentElement";
 
 const Comment = ({ postId }) => {
-    const [commentList, setCommentList] = useState(comments); // state for comments
-    const [newContent, setNewContent] = useState(""); // state for new comment
+  const [commentList, setCommentList] = useState([]); // state for comments
+  const [newContent, setNewContent] = useState(""); // state for new comment
 
-    const handleCommentSubmit = (e) => {
-        e.preventDefault();
-        setCommentList([ // TODO: add api call for creating comment
-            ...commentList,
-            {
-                id: commentList.length + 1,
-                content: newContent,
-                created_at: new Date().toISOString(),
-                post: postId,
-                author: {
-                    id: 1,
-                    username: "user1"
-                }
-            }
-        ]);
-        console.log({
-            post: postId,
-            content: newContent
-        });
-        setNewContent("");
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    setCommentList([
+      // TODO: add api call for creating comment
+      ...commentList,
+      {
+        id: commentList.length + 1,
+        content: newContent,
+        created_at: new Date().toISOString(),
+        post: postId,
+        author: {
+          id: 1,
+          username: "user1",
+        },
+      },
+    ]);
+    console.log({
+      post: postId,
+      content: newContent,
+    });
+    setNewContent("");
+  };
+
+  const handleCommentDelete = (commentId) => {
+    console.log("comment: ", commentId);
+    setCommentList(commentList.filter((comment) => comment.id !== commentId)); // TODO: add api call for deleting comment
+  };
+
+  useEffect(() => {
+    const commentAPI = async (postId) => {
+      const comments = await getComments(postId);
+      setCommentList(comments);
     };
+    commentAPI(postId);
+  }, [postId]);
 
-    const handleCommentDelete = (commentId) => {
-        console.log("comment: ", commentId);
-        setCommentList(commentList.filter((comment) => comment.id !== commentId)); // TODO: add api call for deleting comment
-    };
+  return (
+    <div className="w-full mt-5 self-start">
+      <h1 className="text-3xl font-bold my-5">Comments</h1>
+      {commentList.map((comment) => {
+        return (
+          <CommentElement
+            key={comment.id}
+            comment={comment}
+            handleCommentDelete={handleCommentDelete}
+            postId={postId}
+          />
+        );
+      })}
 
-    return (
-        <div className="w-full mt-5 self-start">
-            <h1 className="text-3xl font-bold my-5">Comments</h1>
-            {commentList.map((comment) => {
-                return (
-                    <CommentElement key={comment.id} comment={comment} handleCommentDelete={handleCommentDelete} postId={postId} />
-                );
-            })}
-            
-            <form className="flex flex-row mt-10 gap-3" onSubmit={handleCommentSubmit}>
-                <input type="text" value={newContent} placeholder="댓글을 입력해주세요" className="input" style={{ width: "calc(100% - 100px)" }} onChange={(e) => setNewContent(e.target.value)} />
-                <button type="submit" className="button">작성</button>
-            </form>
-        </div>
-    );
+      <form
+        className="flex flex-row mt-10 gap-3"
+        onSubmit={handleCommentSubmit}
+      >
+        <input
+          type="text"
+          value={newContent}
+          placeholder="댓글을 입력해주세요"
+          className="input"
+          style={{ width: "calc(100% - 100px)" }}
+          onChange={(e) => setNewContent(e.target.value)}
+        />
+        <button type="submit" className="button">
+          작성
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Comment;
